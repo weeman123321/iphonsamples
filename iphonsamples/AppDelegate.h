f87f4bd79091212aa82e8c64296d1b36390c28e3
Optimind <optimind@Eldriech.local>
2013-08-05 10:00:06 +0800

    Initial Commit

---
+++ iphonsamples/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iphonsamples
+//
+//  Created by Optimind on 8/5/13.
+//  Copyright (c) 2013 Optimind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iphonsamples/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iphonsamples
+//
+//  Created by Optimind on 8/5/13.
+//  Copyright (c) 2013 Optimind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
